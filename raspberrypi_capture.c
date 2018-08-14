b521f310a0cf5a0ec1d6cc661939b710a42e2454
Isaac Qiao <isaac@d207-023-205-100.wireless.sfu.ca>
2018-08-13 20:22:50 -0700

    fixed

--- raspberrypi_capture.c
+++ raspberrypi_capture.c
@@ -234,21 +234,18 @@ int main(int argc, char *argv[])
 	//int loop=0;
 	int temp=0;
 	int count_high=0;
-	while(1){
-		while(transfer(fd)!=59){}
-		temp=print_max_temp();
-		if(temp>10000){
-			count_high++;
-		}
-		if(count_high>10){
-			// critical temp keeps more than 15s
-			// trigger alarm
-			count_high=0;
-			save_pgm_file();	
-		}
-		usleep(500000);
-		//loop++;
+	while(transfer(fd)!=59){}
+	temp=print_max_temp();
+	if(temp>10000){
+		count_high++;
 	}
+	if(count_high>10){
+		// critical temp keeps more than 15s
+		// trigger alarm
+		count_high=0;
+		save_pgm_file();	
+	}
+		//loop++;
 
 	close(fd);
 
